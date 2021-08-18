a6cb972a98d3ad25abb22e0aa8987959f4e6df07
alexsoyes <contact.alexsoyes@gmail.com>
2021-08-18 10:56:32 +0200

    feat: post content mobile-ready

--- inc/table-of-contents.php
+++ inc/table-of-contents.php
@@ -12,7 +12,7 @@ function get_the_replaced_heading( ?array $match ): string {
 	$heading_id = sanitize_title_with_dashes( remove_accents( ( $match[3] ) ) );
 	$svg        = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><title>" . sprintf( __( 'Make a link to: %s', 'soyes' ), $match[3] ) . "</title><path d='M208 352h-64a96 96 0 010-192h64M304 160h64a96 96 0 010 192h-64M163.29 256h187.42' fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='36'/></svg>";
 
-	return "<h$match[1] id=\"$heading_id\"$match[2]><a href=\"#$heading_id\">$svg</a>$match[3]</h$match[1]>";
+	return "<h$match[1] id=\"$heading_id\"$match[2]><a href=\"#$heading_id\" class=\"toc_ref\">$svg</a>$match[3]</h$match[1]>";
 }
 
 function the_content_with_toc( string $content, string $headers = '2,3' ): string {
