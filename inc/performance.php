0f3f310dfb349b0b761cc8f527d8f69a157b66ef
alexsoyes <contact.alexsoyes@gmail.com>
2021-09-05 00:56:17 +0200

    perf: enable WP API

--- inc/performance.php
+++ inc/performance.php
@@ -18,13 +18,5 @@ function soyes_performance_setup(): void {
 
 	remove_action( 'wp_head', 'wp_shortlink_wp_head', 10 );
 
-	// Filters for WP-API version 1.x.
-	add_filter( 'json_enabled', '__return_false' );
-	add_filter( 'json_jsonp_enabled', '__return_false' );
-
-	// Filters for WP-API version 2.x.
-	add_filter( 'rest_enabled', '__return_false' );
-	add_filter( 'rest_jsonp_enabled', '__return_false' );
-
 	remove_action( 'wp_head', 'wc_generator_tag' );
 }
