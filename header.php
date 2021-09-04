bde4e376c8d19d1a054e93cf3e8f6b59f856c963
alexsoyes <contact.alexsoyes@gmail.com>
2021-09-05 00:20:00 +0200

    feat: self hosting open-sans

--- header.php
+++ header.php
@@ -12,10 +12,19 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link rel="preload"
+          href="<?php echo get_home_url( '/' ) ?>/wp-content/themes/soyes/assets/fonts/open-sans-v23-latin-800.woff2"
+          as="font"
+          crossorigin="anonymous"/>
+    <link rel="preload"
+          href="<?php echo get_home_url( '/' ) ?>/wp-content/themes/soyes/assets/fonts/open-sans-v23-latin-700.woff2"
+          as="font"
+          crossorigin="anonymous"/>
+    <link rel="preload"
+          href="<?php echo get_home_url( '/' ) ?>/wp-content/themes/soyes/assets/fonts/open-sans-v23-latin-regular.woff2"
+          as="font"
+          crossorigin="anonymous"/>
 	<?php wp_head(); ?>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700;800&display=swap" rel="stylesheet">
 </head>
 
 <body <?php body_class(); ?>>
