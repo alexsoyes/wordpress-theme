8ee8c9e94b71b6c82e61880d3f1e72605435c5c5
alexsoyes <contact.alexsoyes@gmail.com>
2021-10-30 11:01:35 +0200

    feat: style for modification date, code and author box.

--- template-parts/content/content-post.php
+++ template-parts/content/content-post.php
@@ -27,14 +27,16 @@
 			?>
         </div><!-- .entry-content -->
 
-        <footer id="community" class="entry-footer entry-social">
+        <footer id="community" class="entry-footer">
 		    <?php get_template_part( 'template-parts/elements/author-box' ); ?>
-		    <?php get_template_part( 'template-parts/elements/share' ); ?>
-            <a href="#content" title="<?php esc_html_e( 'Back to top', 'soyes' ); ?>">
-                <img src="<?php echo soyes_get_the_image( 'caret-up-outline', 'svg', true, false ) ?>"
-                     alt="<?php esc_html_e( 'Back to top', 'soyes' ); ?>" width="30" height="30"
-                     aria-hidden="true" class="back-to-top"/>
-            </a>
+            <div class="entry-social">
+			    <?php get_template_part( 'template-parts/elements/share' ); ?>
+                <a href="#content" title="<?php esc_html_e( 'Back to top', 'soyes' ); ?>">
+                    <img src="<?php echo soyes_get_the_image( 'caret-up-outline', 'svg', true, false ) ?>"
+                         alt="<?php esc_html_e( 'Back to top', 'soyes' ); ?>" width="30" height="30"
+                         aria-hidden="true" class="back-to-top"/>
+                </a>
+            </div>
         </footer><!-- .entry-footer -->
 
     </article><!-- #post-<?php the_ID(); ?> -->
