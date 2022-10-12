dc5786e47479d973e67d9dc3321f3303b7b83a8f
alexsoyes <contact.alexsoyes@gmail.com>
2022-10-12 08:46:27 +0800

    feat: wip uvp with 5 emails on homepage

--- inc/shortcodes/newsletter.php
+++ inc/shortcodes/newsletter.php
@@ -7,8 +7,8 @@
  */
 
 add_shortcode(
-	'soyes_newsletter',
-	'soyes_newsletter'
+    'soyes_newsletter',
+    'soyes_newsletter'
 );
 
 /**
@@ -16,50 +16,78 @@ add_shortcode(
  *
  * @return string
  */
-function soyes_newsletter( $atts = array() ): string {
-	$container_class = 'soyes-newsletter-container';
+function soyes_newsletter($atts = array()): string
+{
+    $container_class = 'soyes-newsletter-container';
 
-	$additional_class = is_array( $atts ) && array_key_exists( 'class', $atts ) ? $atts['class'] : false;
+    $additional_class = is_array($atts) && array_key_exists('class', $atts) ? $atts['class'] : false;
 
-	if ( $additional_class ) {
-		$container_class = "$container_class $additional_class";
-	}
+    if ($additional_class) {
+        $container_class = "$container_class $additional_class";
+    }
 
-	ob_start();
-	?>
-    <div class="<?php echo esc_html( $container_class ); ?>">
-        <img src="<?php echo soyes_get_the_image( 'logo-only', 'svg', false, false ); ?>"
-             alt="<?php echo get_bloginfo( 'name' ); ?>"
-             class="soyes-newsletter-icon"
-             width="50" height="50">
+    ob_start();
+    ?>
+    <div class="<?php echo esc_html($container_class); ?>">
+        <div class="has-text-align-center">
+            <img src="<?php echo soyes_get_the_image('logo-only', 'svg', false, false); ?>"
+                 alt="<?php echo get_bloginfo('name'); ?>"
+                 class="soyes-newsletter-icon"
+                 width="50" height="50">
 
-        <p class="soyes-newsletter-title">
-		    <?php
-			_e(
-				'1 unique advice<br>
-                <span>every month</span>.',
-				'soyes'
-			);
-			?>
-        </p><!-- .soyes-newsletter-title -->
+            <p class="soyes-newsletter-title">
+                <?php
+                _e(
+                    'Receive <span>&lt;la console /&gt;</span>.',
+                    'soyes'
+                );
+                ?>
+            </p><!-- .soyes-newsletter-title -->
+
+            <p class="soyes-newsletter-desc">
+                <?php _e('✨ Being developer is a passion.', 'soyes'); ?>
+            </p><!-- . soyes-newsletter-desc -->
+
+
+        </div><!-- .has-text-align-center -->
 
         <div class="soyes-newsletter-content">
-            <p class="soyes-newsletter-desc">
-			    <?php _e( 'Each beginning of the month, receive a unique advice to <strong>improve your developer skills</strong>, for free.', 'soyes' ); ?>
-            </p><!-- . soyes-newsletter-desc -->
-            <form class="soyes-newsletter-form"
-                  action="https://alexsoyes.us8.list-manage.com/subscribe/post?u=987967b6e21378d1da9bd507b&amp;id=1984a69e0e"
-                  method="post">
-                <input name="EMAIL" type="email" placeholder="<? _e( 'My email address', 'soyes' ); ?>" required
-                       aria-label="Adresse e-mail" class="soyes-newsletter-email">
-                <input type="submit" class="wp-block-button__link soyes-newsletter-submit"
-                       value="<?php esc_html_e( 'I want to receive the tips', 'soyes' ); ?>">
-            </form><!-- .soyes-newsletter-form -->
-            <small class="soyes-newsletter-warn">
-                <?php esc_html_e( '* Your email address will NEVER be used by someone other than me.', 'soyes' ); ?>
-            </small><!-- .soyes-newsletter-warn -->
+
+            <div class="wp-block-columns is-column-reversed-on-mobile">
+                <div class="wp-block-column wp-block-column-33 soyes-newsletter-fill">
+                    <p><?php _e('<span>Each beginning</span> of the week,', 'soyes'); ?></p>
+                    <p><?php _e('I will share you <span>something I learned</span>.', 'soyes'); ?></p>
+
+                    <small><?php _e('🎁 <span>Bonus gift</span>: Receive my 5 bonus emails to become a better software developer.', 'soyes'); ?></small>
+
+                    <form class="soyes-newsletter-form"
+                          action="https://alexsoyes.us8.list-manage.com/subscribe/post?u=987967b6e21378d1da9bd507b&amp;id=1984a69e0e"
+                          method="post">
+                        <input name="EMAIL" type="email" placeholder="<? _e('My email address', 'soyes'); ?>" required
+                               aria-label="Adresse e-mail" class="soyes-newsletter-email">
+                        <input type="submit" class="wp-block-button__link soyes-newsletter-submit"
+                               value="<?php _e('Join the community!', 'soyes'); ?>">
+                    </form><!-- .soyes-newsletter-form -->
+                </div><!-- .soyes-newsletter-fill -->
+
+                <div class="wp-block-column wp-block-column-66 soyes-newsletter-advantages">
+                    <ul class="simple">
+                        <li><?php _e('🗺️ <strong>3 Roadmaps</strong> to became a better software developer.', 'soyes'); ?></li>
+                        <li><?php _e('🧠 How I start my day to <strong>increase my productivity</strong>.', 'soyes'); ?></li>
+                        <li><?php _e('🛠 <strong>3 tools</strong> for developers (that you must use).', 'soyes'); ?></li>
+                        <li><?php _e('🧑‍💻 What I think about <strong>software engineers jobs</strong> for the next year.', 'soyes'); ?></li>
+                        <li><?php _e('😍 A curated selection of the <strong>best french ressources</strong> for coding.', 'soyes'); ?></li>
+                    </ul><!-- .simple -->
+
+                    <small class="soyes-newsletter-warn">
+                        <?php _e('* Your email stays with me, your privacy matters. 🕵️', 'soyes'); ?>
+                    </small><!-- .soyes-newsletter-warn -->
+
+                </div><!-- .soyes-newsletter-advantages -->
+            </div><!-- .wp-block-columns -->
         </div><!-- .soyes-newsletter-content -->
+
     </div><!-- .soyes-newsletter-container -->
-	<?php
-	return ob_get_clean();
+    <?php
+    return ob_get_clean();
 }
