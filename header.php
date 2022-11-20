e57e27ce44d8f4bdd74717a54f130c1dd46d9fc0
alexsoyes <contact.alexsoyes@gmail.com>
2022-11-20 14:22:15 +0700

    feat(banner): New banner for promotion.

--- header.php
+++ header.php
@@ -10,53 +10,63 @@
 <!doctype html>
 <html <?php language_attributes(); ?> itemscope itemtype="http://schema.org/WebPage">
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>"/>
+    <meta charset="<?php bloginfo('charset'); ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
-	<?php if ( is_single() || is_front_page() || is_page() || is_404() ) : ?>
+    <?php if (is_single() || is_front_page() || is_page() || is_404()) : ?>
         <link rel="prefetch"
-              href="<?php echo esc_url( wp_get_attachment_url( get_theme_mod( 'custom_logo' ) ) ); ?>"
+              href="<?php echo esc_url(wp_get_attachment_url(get_theme_mod('custom_logo'))); ?>"
               crossorigin="anonymous"/>
-	<?php endif; ?>
-	<?php wp_head(); ?>
+    <?php endif; ?>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-    <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'soyes' ); ?></a>
+    <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'soyes'); ?></a>
 
-	<?php if ( ! is_front_page() ) : ?>
+    <div class="new-banner alignfull">
+        <span class="new-icon"><?php esc_attr_e('New', 'soyes'); ?></span><!-- .promo-new -->
+        <span class="new-content"><?php _e('Training to become a freelance with <strong>30&percnt; discount!</strong>', 'soyes'); ?></span>
+        <!-- .promo-content -->
+        <span class="new-cta">
+            <button class="wp-block-button__link"
+                    onclick='window.open("https://bit.ly/3tLbRqc", "_blank")'>
+                <?php esc_attr_e('Access ✨', 'soyes'); ?>
+            </button>
+        </span><!-- .promo-cta -->
+    </div><!-- .header-promo -->
 
+    <?php if (!is_front_page()) : ?>
         <header id="header" class="alignfull">
             <div class="header-container">
-				<?php if ( has_custom_logo() ) : ?>
+                <?php if (has_custom_logo()) : ?>
                     <div class="site-logo"><?php the_custom_logo(); ?></div><!-- .site-logo -->
-				<?php else : ?>
+                <?php else : ?>
                     <a href="<?php echo home_url(); ?>" id="site-home">
-                        <span class="site-logo-text"><?php echo get_bloginfo( 'name' ); ?></span><!-- .site-logo-text -->
+                        <span class="site-logo-text"><?php echo get_bloginfo('name'); ?></span><!-- .site-logo-text -->
                     </a><!-- #site-home -->
-				<?php endif; ?>
+                <?php endif; ?>
 
-				<?php if ( has_nav_menu( 'primary' ) ) : ?>
+                <?php if (has_nav_menu('primary')) : ?>
                     <nav id="site-navigation" class="primary-navigation"
-                         aria-label="<?php esc_attr_e( 'Primary menu', 'soyes' ); ?>">
-						<?php
-						wp_nav_menu(
-							array(
-								'theme_location'  => 'primary',
-								'menu_class'      => 'menu-wrapper',
-								'container_class' => 'primary-menu-container',
-								'items_wrap'      => '<ul id="primary-menu-list" class="%2$s">%3$s</ul>',
-								'fallback_cb'     => false,
-							)
-						);
-						?>
+                         aria-label="<?php esc_attr_e('Primary menu', 'soyes'); ?>">
+                        <?php
+                        wp_nav_menu(
+                            array(
+                                'theme_location' => 'primary',
+                                'menu_class' => 'menu-wrapper',
+                                'container_class' => 'primary-menu-container',
+                                'items_wrap' => '<ul id="primary-menu-list" class="%2$s">%3$s</ul>',
+                                'fallback_cb' => false,
+                            )
+                        );
+                        ?>
                     </nav><!-- #site-navigation -->
-				<?php endif; ?>
+                <?php endif; ?>
             </div><!--. header-container -->
         </header><!-- #header -->
-
-	<?php endif; ?>
+    <?php endif; ?>
 
     <div id="content" class="site-content">
         <div id="primary" class="content-area">
