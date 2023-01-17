0033fc901bad537dab694d69842ab33e3f90c2e2
alexsoyes <contact.alexsoyes@gmail.com>
2023-01-17 06:45:40 +0100

    feat(homepage): Change taglines.

--- inc/shortcodes/newsletter.php
+++ inc/shortcodes/newsletter.php
@@ -38,29 +38,27 @@ function soyes_newsletter($atts = []): string
             <p class="soyes-newsletter-title">
                 <?php
                 _e(
-                    'Chaque semaine,<br><span>1 conseil pour mieux coder</span>.',
+                    '1 action chaque semaine<br><span>pour rejoindre le top 10 % des devs</span>.',
                     'soyes'
                 );
                 ?>
             </p><!-- .soyes-newsletter-title -->
 
             <p class="soyes-newsletter-desc">
-                <strong><?php _e("💌 La console : La newsletter des devs.", 'soyes'); ?></strong>
+                <strong><?php _e("🚀 La console, la newsletter 100 % dev, freelance et digital nomad.", 'soyes'); ?></strong>
             </p><!-- . soyes-newsletter-desc -->
-
         </div><!-- .has-text-align-center -->
-
 
         <div class="soyes-newsletter-content">
             <div class="soyes-newsletter-fill">
                 <form class="soyes-newsletter-form wp-block-columns"
                       action="https://alexsoyes.us8.list-manage.com/subscribe/post?u=987967b6e21378d1da9bd507b&amp;id=1984a69e0e"
                       method="post">
-                    <input name="EMAIL" type="email" placeholder="<? _e('Rejoindre +1500 membres', 'soyes'); ?>"
+                    <input name="EMAIL" type="email" placeholder="<? _e('Mon adresse e-mail principale', 'soyes'); ?>"
                            required
                            aria-label="Adresse e-mail" class="soyes-newsletter-email wp-block-column">
                     <input type="submit" class="wp-block-button__link soyes-newsletter-submit wp-block-column"
-                           value="<?php _e('Je m’abonne 🚀', 'soyes'); ?>">
+                           value="<?php _e("Je rejoins les + 1500 membres", 'soyes'); ?>">
                 </form><!-- .soyes-newsletter-form -->
 
                 <small class="soyes-newsletter-warn">
