cfc11f1d687d6508848c98e5ee5380b7429d242b
alexsoyes <contact.alexsoyes@gmail.com>
2023-01-24 14:38:28 +0800

    feat(content): WIP tocbot integration

--- functions.php
+++ functions.php
@@ -184,6 +184,10 @@ function soyes_scripts(): void
     if (is_single()) {
         wp_enqueue_style('soyes-style-single', get_template_directory_uri() . '/assets/css/parts/single.css', array(), $version);
         wp_enqueue_script('soyes-script-share', get_template_directory_uri() . '/assets/js/share.js', array(), $version, true);
+
+        // toc
+        wp_enqueue_style('soyes-style-toc-theme', get_template_directory_uri() . '/assets/modules/tocbot-4.12.0/dist/tocbot.css', array(), $version);
+        wp_enqueue_script('soyes-script-toc', get_template_directory_uri() . '/assets/modules/tocbot-4.12.0/dist/tocbot.min.js', array(), $version, true);
     }
 
     if (!is_front_page()) {
@@ -213,12 +217,6 @@ function soyes_scripts(): void
 
     // included everywhere because is in footer.
     wp_enqueue_style('soyes-style-shortcode-newsletter', get_template_directory_uri() . '/assets/css/shortcodes/newsletter.css', array(), $version);
-
-    if ($post) {
-        if (is_single() && has_shortcode($post->post_content, 'soyes_toc')) {
-            wp_enqueue_style('soyes-style-shortcode-toc', get_template_directory_uri() . '/assets/css/shortcodes/toc.css', array(), $version);
-        }
-    }
 
     wp_deregister_style('wp-block-library');
     wp_deregister_script('wp-block-library');
@@ -311,3 +309,4 @@ function soyes_hotjar(): void
 }
 
 add_action('wp_head', 'soyes_hotjar');
+
--- template-parts/content/content-post.php
+++ template-parts/content/content-post.php
@@ -9,40 +9,47 @@
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
         <header class="entry-header">
-			<?php get_template_part( 'template-parts/post/post-header' ); ?>
+            <?php get_template_part('template-parts/post/post-header'); ?>
         </header><!-- .entry-header -->
 
         <div class="entry-share">
             <div class="link-share">
                 <div class="link-share-buttons">
-                    <?php get_template_part( 'template-parts/elements/share-icons' ); ?>
+                    <?php get_template_part('template-parts/elements/share-icons'); ?>
                 </div><!-- .link-share-buttons -->
             </div><!-- .link-share -->
         </div><!-- .entry-share -->
 
         <div class="entry-content">
-			<?php
-			the_content();
-
-			wp_link_pages(
-				array(
-					'before'   => '<nav class="page-links" aria-label="' . esc_attr__( 'Page', 'soyes' ) . '">',
-					'after'    => '</nav>',
-					/* translators: %: Page number. */
-					'pagelink' => esc_html__( 'Page %', 'soyes' ),
-				)
-			);
-			?>
+            <div class="wp-block-columns is-column-reversed-on-mobile">
+                <div class="wp-block-column" id="column-content">
+                    <?php the_content(); ?>
+                </div><!-- .wp-block-column -->
+                <div class="wp-block-column is-relative">
+                    <input type="checkbox" id="toc" class="toc-input">
+                    <label class="toc-icon" for="toc">☰</label>
+                    <div id="column-toc" class="toc toc-right"></div>
+                </div><!-- .wp-block-column -->
+            </div><!-- .wp-block-columns -->
+            <?php
+            wp_link_pages(
+                array(
+                    'before' => '<nav class="page-links" aria-label="' . esc_attr__('Page', 'soyes') . '">',
+                    'after' => '</nav>',
+                    /* translators: %: Page number. */
+                    'pagelink' => esc_html__('Page %', 'soyes'),
+                )
+            );
+            ?>
         </div><!-- .entry-content -->
 
         <footer class="entry-footer">
             <div id="community" class="entry-social">
-			    <?php get_template_part( 'template-parts/elements/share' ); ?>
-                <a href="#page" title="<?php esc_html_e( 'Back to top', 'soyes' ); ?>">
-                    <img src="<?php echo soyes_get_the_image( 'caret-up-outline', 'svg', true, false ) ?>"
-                         alt="<?php esc_html_e( 'Back to top', 'soyes' ); ?>" width="30" height="30"
-                         aria-hidden="true" class="back-to-top"/>
+                <?php get_template_part('template-parts/elements/share'); ?>
+                <a href="#page" title="<?php esc_html_e('Back to top', 'soyes'); ?>">
+                    <span class="back-to-top">☝️</span><!-- .back-to-top -->
                 </a>
+
             </div>
         </footer><!-- .entry-footer -->
 
@@ -50,6 +57,6 @@
 
 <?php
 
-if ( comments_open() || get_comments_number() ) {
-	comments_template();
+if (comments_open() || get_comments_number()) {
+    comments_template();
 }
