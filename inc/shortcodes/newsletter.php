34645de6bf22794994f454a60c0b056af0f5c2fb
alexsoyes <contact.alexsoyes@gmail.com>
2023-01-25 14:17:05 +0800

    feat(header): Change hero color

--- inc/shortcodes/newsletter.php
+++ inc/shortcodes/newsletter.php
@@ -38,14 +38,14 @@ function soyes_newsletter($atts = []): string
             <p class="soyes-newsletter-title">
                 <?php
                 _e(
-                    '🚀 La console : La newsletter <span>#code</span> et <span>#freelance</span>',
+                    '🚀 La console, la newsletter <span>#code</span> et <span>#freelance</span>.',
                     'soyes'
                 );
                 ?>
             </p><!-- .soyes-newsletter-title -->
 
             <p class="soyes-newsletter-desc">
-                <strong><?php _e("✅ 1 action à réaliser chaque lundi pour décrocher le job de tes rêves ou devenir freelance.", 'soyes'); ?></strong>
+                <strong><?php _e("✅ 1 action à réaliser chaque lundi à 10h", 'alexsoyes'); ?></strong> <?php _e("pour décrocher le job de tes rêves ou devenir freelance.", 'soyes'); ?>
             </p><!-- . soyes-newsletter-desc -->
         </div><!-- .has-text-align-center -->
 
