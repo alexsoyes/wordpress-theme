60a84d997e74c6535c8ede6556fb50b7e4188f35
alexsoyes <contact.alexsoyes@gmail.com>
2023-03-16 08:54:43 +0700

    feat(tracking): Make GTM async

--- functions.php
+++ functions.php
@@ -227,6 +227,8 @@ function soyes_scripts(): void
 
     wp_enqueue_script('soyes-script-newsletter', get_template_directory_uri() . '/assets/js/newsletter.js', array(), $version, true);
 
+    wp_enqueue_script('soyes-script-gtm', get_template_directory_uri() . '/assets/js/gtm.js', array(), $version, true);
+
     // included everywhere because is in footer.
     wp_enqueue_style('soyes-style-shortcode-newsletter', get_template_directory_uri() . '/assets/css/shortcodes/newsletter.css', array(), $version);
 
