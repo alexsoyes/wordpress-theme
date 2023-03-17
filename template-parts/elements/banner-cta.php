430d56bb75ea5b41a1fde11fc9dca471a43366cb
alexsoyes <contact.alexsoyes@gmail.com>
2023-03-17 16:24:43 +0700

    feat(banner): New course with deadline

--- template-parts/elements/banner-cta.php
+++ template-parts/elements/banner-cta.php
@@ -1,7 +1,7 @@
 <aside class="new-banner">
     <span class="new-content"><?php _e('<strong>🆕 Cours gratuit : Devenir dev freelance</strong> (Comment se lancer en toute sécurité)', 'soyes'); ?></span>
     <span class="new-cta">
-        <span onclick='window.open("https://learn.alexsoyes.com/cours-devenir-freelance?utm_source=blog&utm_medium= banner-cta&utm_campaign=cours-gratuit-devenir-dev-freelance-comment-se-lancer-en-toute-securite&utm_content=rejoindre-la-liste-dattente", "_blank")'>
+        <span onclick='window.open("https://learn.alexsoyes.com/cours-devenir-freelance-6f2f4b3a?utm_source=blog&utm_medium= banner-cta&utm_campaign=cours-gratuit-devenir-dev-freelance-comment-se-lancer-en-toute-securite&utm_content=rejoindre-la-liste-dattente", "_blank")'>
             <?php esc_attr_e("Rejoindre la liste d'attente 🚀", 'soyes'); ?>
         </span><!-- .wp-block-button__link -->
     </span><!-- .new-cta -->
