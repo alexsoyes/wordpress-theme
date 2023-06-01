3372fd0de5099d96eced52d316213aece57214b7
alexsoyes <contact.alexsoyes@gmail.com>
2023-06-01 11:11:27 +0200

    fix(ux): Order posts by title

--- inc/post-order.php
+++ inc/post-order.php
@@ -3,10 +3,11 @@
 /**
  * @param WP_Query $query
  */
-function soyes_posts_ordering( WP_Query $query ): void {
-	if ( is_archive() || is_home() ) {
-		$query->set( 'orderby', 'modified' );
-	}
+function soyes_posts_ordering(WP_Query $query): void
+{
+    if (is_archive() || is_home()) {
+        $query->set('orderby', 'post_title');
+    }
 }
 
-add_filter( 'pre_get_posts', 'soyes_posts_ordering' );
+add_filter('pre_get_posts', 'soyes_posts_ordering');
