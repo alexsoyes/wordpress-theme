a41513a4f6b54afa6500fd010e0b09c4fba01cf2
alexsoyes <contact.alexsoyes@gmail.com>
2023-09-04 00:04:05 +0200

    fix: remove default WordPress style

--- functions.php
+++ functions.php
@@ -242,6 +242,7 @@ function soyes_scripts(): void
     // included everywhere because is in footer.
     wp_enqueue_style('soyes-style-shortcode-newsletter', get_template_directory_uri() . '/assets/css/shortcodes/newsletter.css', array(), $version);
 
+    wp_dequeue_style('classic-theme-styles');
     wp_dequeue_style('global-styles');
     wp_deregister_style('wp-block-library');
     wp_deregister_script('wp-block-library');
